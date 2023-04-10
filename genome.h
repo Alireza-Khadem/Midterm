0b98b607ac25809fc9c350e68d152b65ec45698c
Alireza-Khadem <akhasdf1383@gmail.com>
2023-04-10 18:29:55 +0430

    genome.h

--- genome.h
+++ genome.h
@@ -1,168 +1,272 @@
-//genome header
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <algorithm>
 using namespace std;
-//in function baraye reverse kardan reshte estafade mishavad
-string reverse(string a){
-    string temp;
-    for(int i = a.length() - 1;i >= 0;i--){
-        temp += a[i];
-    }
-    return temp;
-}
-//tarif class genome
-class Genome{
-private:
-    string RNA = "";
-    vector<string> DNA;
-public:
-    //setter rna & dna
-    void Setter(string RNA,string DNA_1,string DNA_2){
-        this->RNA = RNA;
-        this->DNA = {DNA_1,DNA_2};
-    }
-    //bargardandan rna
-    string GetRNA(){
-        return this->RNA;
-    }
-    //bargardandan dna
-    vector<string> GetDNA(){
-        return this->DNA;
-    }
-    //sakht dna az rna
-    string DNA_Maker(string RNA){
-        //char ha ra yeki yeki mokamel mishavad
-        string temp = "";
-        for(int i = 0;i < RNA.length();i++){
-            if(RNA[i] == 'A'){
-                temp += 'T';
-            }
-            else if(RNA[i] == 'T'){
-                temp += 'A';
-            }
-            else if(RNA[i] == 'C'){
-                temp += 'G';
-            }
-            else if(RNA[i] == 'G'){
-                temp += 'C';
-            }
-        }
-        return temp;
-    }
-    //method jahesh koockak
-    void S_Mut(string _NA,char c1,char c2,int num){
-        //daryaft rna\dna va char aval va char dovom va tedad
-        int counter = 0;
-        //agar rna bood vared in shart mishavad
-        if(_NA == "RNA"){
-            for(int i = 0;i < RNA.length();i++){
-                if(RNA[i] == c1){
-                    RNA[i] = c2;
-                    counter++;
-                }
-                if(counter == num){
-                    cout << "Small Mutation Done!" << endl;
-                    break;
-                }
-            }
-        }
-        //agar dna bood vared in shart mishavad
-        else if(_NA == "DNA"){
-            char j;
-            switch(c2){
-                case 'T':
-                    j = 'A';
-                    break;
-                case 'A':
-                    j = 'T';
-                    break;
-                case 'G':
-                    j = 'C';
-                    break;
-                case 'C':
-                    j = 'G';
-                    break;
-            }
-            for(int i = 0;i < DNA[0].length();i++){
-                if(DNA[0][i] == c1){
-                    DNA[0][i] = c2;
-                    DNA[1][i] = j;
-                    counter++;
-                }
-                else if(DNA[1][i] == c1){
-                    DNA[1][i] = c2;
-                    DNA[0][i] = j;
-                    counter++;
-                }
-                if(counter == num){
-                    cout << "Small Mutation Done!" << endl;
-                    break;
-                }
-            }
-        }
-    }
-    //method jahesh bozorg
-    void B_Mut(string t,string word,string input){
-        //daryaft rna\dna va ebarat aval va dovom
-        string temp;
-        //agar dna bood vared in shart mishavad
-        if(t == "DNA"){
-            if(DNA[0].find(word) != -1){
-                int index = DNA[0].find(word);
-                DNA[0].replace(index,word.length(),input);
-                DNA[1].replace(index,word.length(), DNA_Maker(input));
-                cout << "Big Mutation Done!" << endl;
-            }
-            else if(DNA[1].find(word) != -1){
-                int index = DNA[1].find(word);
-                DNA[1].replace(index,word.length(),input);
-                DNA[0].replace(index,word.length(), DNA_Maker(input));
-                cout << "Big Mutation Done!" << endl;
-            }
-            else{
-                cout << "No Match!" << endl;
-            }
-        }
-        //agar rna bood vared in shart mishavad
-        else{
-            if(RNA.find(word) != -1){
-                RNA.replace(RNA.find(word),word.length(),input);
-                cout << "Big Mutation Done!" << endl;
-            }
-            else{
-                cout << "No Match!" << endl;
-            }
-        }
-    }
-    //method jahesh makoos
-    void R_Mut(string t,string word){
-        //daryaft rna\dna va ebarat
-        if(t == "RNA"){
-            int index = RNA.find(word);
-            if(index != -1){
-                RNA.replace(index,word.length(), reverse(word));
-                cout << "Reverse Mutation Done!" << endl;
-            }
-            else{
-                cout << "No Match!" << endl;
-            }
-        }
-        else{
-            if(DNA[0].find(word) != -1){
-                int index = DNA[0].find(word);
-                DNA[0].replace(index,word.length(), reverse(word));
-                DNA[1].replace(index,word.length(), reverse(DNA_Maker(word)));
-                cout << "Reverse Mutation Done!" << endl;
-            }
-            else if(DNA[1].find(word) != -1){
-                int index = DNA[1].find(word);
-                DNA[1].replace(index,word.length(), reverse(word));
-                DNA[0].replace(index,word.length(), reverse(DNA_Maker(word)));
-                cout << "Reverse Mutation Done!" << endl;
-            }
-            else{
-                cout << "No Match!" << endl;
-            }
-        }
-    }
+
+struct DNAA
+{
+    string first;
+    string second;
 };
+
+class Genome
+{
+private:
+    string RNA;
+    DNAA DNA;
+
+public:
+    Genome();
+    Genome(string, DNAA );
+    void setter(string, DNAA);
+    string getRNA();
+    DNAA getDNA();
+    void createDNAfromRNA();
+    void smallJump(char, char, int);
+    void bigJump(string, string);
+    void reverseJump(string);
+};
+
+Genome::Genome()
+{
+    this->RNA = "";
+    this->DNA.first = "";
+    this->DNA.second = "";
+}
+
+Genome::Genome(string RNA, DNAA DNA)
+{
+    this->RNA = RNA;
+    this->DNA = DNA;
+}
+
+void Genome::setter(string RNA, DNAA DNA)
+{
+    this->RNA = RNA;
+    this->DNA = DNA;
+}
+
+string Genome::getRNA()
+{
+    return this->RNA;
+}
+
+DNAA Genome::getDNA()
+{
+    return this->DNA;
+}
+
+void Genome::createDNAfromRNA()
+{
+    string resultDNA = "";
+
+    for(int i = 0; i < RNA.length(); i++)
+    {
+        if(RNA[i] == 'A')
+        {
+            resultDNA += 'T';
+        }
+        else if(RNA[i] == 'T')
+        {
+            resultDNA += 'A';
+        }
+        else if(RNA[i] == 'C')
+        {
+            resultDNA += 'G';
+        }
+        else if(RNA[i] == 'G')
+        {
+            resultDNA += 'C';
+        }
+    }
+
+    cout << "DNA: " << resultDNA << endl;
+}
+
+void Genome::smallJump(char A,char C,int n)
+{
+    for (int i = 0, c = 0; c < n && i < RNA.length(); i++)
+    {
+        if (RNA[i] == A)
+        {
+            RNA[i] = C;
+        }
+
+        if (DNA.first[i] == A)
+        {
+            DNA.first[i] = C;
+            if (C == 'A')
+            {
+                DNA.second[i] = 'T';
+            }
+            else if (C == 'T')
+            {
+                DNA.second[i] = 'A';
+            }
+            else if (C == 'C')
+            {
+                DNA.second[i] = 'G';
+            }
+            else if (C == 'G')
+            {
+                DNA.second[i] = 'C';
+            }
+        }
+
+        if (DNA.second[i] == A)
+        {
+            DNA.second[i] = C;
+            if (C == 'A')
+            {
+                DNA.first[i] = 'T';
+            }
+            else if (C == 'T')
+            {
+                DNA.first[i] = 'A';
+            }
+            else if (C == 'C')
+            {
+                DNA.first[i] = 'G';
+            }
+            else if (C == 'G')
+            {
+                DNA.first[i] = 'C';
+            }
+        }
+    }
+}
+
+void Genome::bigJump(string s1, string s2)
+{
+    if(RNA.find(s1) != string::npos)
+    {
+        int index = RNA.find(s1);
+        RNA.replace(index, s1.length(), s2);
+    }
+
+    for (int i = 0; i < DNA.first.length(); i++)
+    {
+        if (DNA.first.substr(i, s1.length()) == s1)
+        {
+            DNA.first.replace(i, s1.length(), s2);
+
+            for (int j = 0; j < s2.length(); j++)
+            {
+                if (s2[j] == 'A')
+                {
+                    DNA.second[i+j] = 'T';
+                }
+                else if (s2[j] == 'T')
+                {
+                    DNA.second[i+j] = 'A';
+                }
+                else if (s2[j] == 'C')
+                {
+                    DNA.second[i+j] = 'G';
+                }
+                else
+                {
+                    DNA.second[i+j] = 'C';
+                }
+            }
+
+            break;
+        }
+
+        if (DNA.second.substr(i, s1.length()) == s1)
+        {
+            DNA.second.replace(i, s1.length(), s2);
+
+            for (int j = 0; j < s2.length(); j++)
+            {
+                if (s2[j] == 'A')
+                {
+                    DNA.first[i+j] = 'T';
+                }
+                else if (s2[j] == 'T')
+                {
+                    DNA.first[i+j] = 'A';
+                }
+                else if (s2[j] == 'C')
+                {
+                    DNA.first[i+j] = 'G';
+                }
+                else
+                {
+                    DNA.first[i+j] = 'C';
+                }
+            }
+
+            break;
+        }
+    }
+}
+
+void Genome::reverseJump(string s1)
+{
+    if (RNA.find(s1) != string::npos)
+    {
+        int index = RNA.find(s1);
+        reverse(s1.begin(), s1.end());
+        RNA.replace(index, s1.length(), s1);
+    }
+
+    for (int i = 0; i < DNA.first.length(); i++)
+    {
+        if (DNA.first.substr(i, s1.length()) == s1)
+        {
+            reverse(s1.begin(), s1.end());
+            DNA.first.replace(i, s1.length(), s1);
+
+            for (int j = 0; j < s1.length(); j++)
+            {
+                if (s1[j] == 'A')
+                {
+                    DNA.second[i+j] = 'T';
+                }
+                else if (s1[j] == 'T')
+                {
+                    DNA.second[i+j] = 'A';
+                }
+                else if (s1[j] == 'C')
+                {
+                    DNA.second[i+j] = 'G';
+                }
+                else
+                {
+                    DNA.second[i+j] = 'C';
+                }
+            }
+
+            break;
+        }
+
+        if (DNA.second.substr(i, s1.length()) == s1)
+        {
+            reverse(s1.begin(), s1.end());
+            DNA.second.replace(i, s1.length(), s1);
+
+            for (int j = 0; j < s1.length(); j++)
+            {
+                if (s1[j] == 'A')
+                {
+                    DNA.first[i+j] = 'T';
+                }
+                else if (s1[j] == 'T')
+                {
+                    DNA.first[i+j] = 'A';
+                }
+                else if (s1[j] == 'C')
+                {
+                    DNA.first[i+j] = 'G';
+                }
+                else
+                {
+                    DNA.first[i+j] = 'C';
+                }
+            }
+
+            break;
+        }
+    }
+}
